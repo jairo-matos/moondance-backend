c5dec3d10850622c3a20f42df84c5b6e3508221e
jairo <jairo.pauluz.matos@gmail.com>
2025-08-05 19:02:44 -0400

    feat(api): add profile update endpoint
    - Add PUT method to /person/me for updating user profile
    - Support first_name and last_name field updates
    - Include proper validation and authentication
    - Return updated person data after successful save

--- flask/app/views/person.py
+++ flask/app/views/person.py
@@ -1,8 +1,11 @@
 from flask_restx import Namespace, Resource
-from app.helpers.response import get_success_response
+from flask import request
+from app.helpers.response import get_success_response, parse_request_body, validate_required_fields
 from app.helpers.decorators import login_required
+from common.app_config import config
+from common.services import PersonService
 
-# Create the organization blueprint
+# Create the person blueprint
 person_api = Namespace('person', description="Person-related APIs")
 
 
@@ -11,4 +14,29 @@ class Me(Resource):
     
     @login_required()
     def get(self, person):
-        return get_success_response(person=person)
+        return get_success_response(person=person.as_dict())
+    
+    @person_api.expect(
+        {'type': 'object', 'properties': {
+            'first_name': {'type': 'string'},
+            'last_name': {'type': 'string'}
+        }}
+    )
+    @login_required()
+    def put(self, person):
+        parsed_body = parse_request_body(request, ['first_name', 'last_name'])
+        validate_required_fields(parsed_body)
+        
+        person_service = PersonService(config)
+        
+        # Update person fields
+        person.first_name = parsed_body['first_name']
+        person.last_name = parsed_body['last_name']
+        
+        # Save updated person
+        updated_person = person_service.save_person(person)
+        
+        return get_success_response(
+            message="Profile updated successfully.",
+            person=updated_person.as_dict()
+        )
